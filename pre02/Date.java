2a195300e36a727f15015a750c35c57b025166db
RosaneWeiss <67014774+RosaneWeiss@users.noreply.github.com>
2020-06-19 16:41:35 -0400

    exercize 2 chapter 2

---
+++ pre02/Date.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
+string day;
+int date,month,year;
+day = Friday;
+date = 19;
+month = 6;
+year = 2020;
+
+System.out.println("date =", date);
+System.out.println("month =", month);
+System.out.println("year =", year);
+System.out.println("day =", day);
