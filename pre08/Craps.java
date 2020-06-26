0173e0e312131b9f3033f601676e2ad5705ea4db
RosaneWeiss <67014774+RosaneWeiss@users.noreply.github.com>
2020-06-26 08:27:56 -0400

    Implemented rounds

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -5,24 +5,38 @@ public class Craps {
     public static void main(String[] args){
        
 	   // craps game
-	   //Scanner in = new Scanner(System.in);
+	   Scanner in = new Scanner(System.in);
+	   int rounds = 1;
+		System.out.print("Input rounds :  ");
+		rounds = in.nextInt();
 		int point = 0;
 		String winLose = "Lose";
-		winLose = round(winLose);
-		System.out.println(winLose);
-		while(winLose == "Keep playing"){
-			winLose = secondround(point,winLose) ; 
-			System.out.println("You " + winLose);
-    }
-	}
-	public static String round(String winLose) {
-		int point = 0;
+		while (rounds > 0)
+		{
 		point = shoot(point);
+		winLose = round(winLose,point);
+		System.out.println(winLose);
+		//while (rounds > 0)
+		//{
+			System.out.println("NEW ROUND");
+			while(winLose == "Keep playing"){
+				winLose = secondround(point,winLose) ; 
+				System.out.println("You " + winLose);
+			}
+		
+		rounds = rounds - 1;
+		System.out.println(" ");
+		}
+		
+	}
+	public static String round(String winLose, int point) {
+		//int point = 0;
+		//point = shoot(point);
 		//System.out.println("You rolled a " + point); 
 		if (point == 2 || point == 3 || point ==12) {
-			winLose = "Loose";  
+			winLose = "LOSE";  
 		} else if (point == 7 || point == 11) {
-			winLose = "Win";
+			winLose = "WIN";
 		} else {
 			winLose = "Keep playing";	
 		}
@@ -41,13 +55,16 @@ public class Craps {
 	   
 	  public static String secondround(int point, String winLose) {
 		int rolled = 0;
+		int needed = point;
 		//String winLose = "Kep playing";
 		rolled = shoot(rolled);
-		//System.out.println("You rolled a " + rolled); 
+		System.out.println("You need a " + needed);
+		System.out.println("You rolled a " + rolled); 
 		if (rolled == 7 ) {
-			winLose = "Loose";  
-		} else if (rolled == point) {
-			winLose = "Win";
+			winLose = "LOSE";  
+		//} else if (rolled == point) {
+			} else if (rolled == needed) {
+			winLose = "WIN";
 		} 
 		return winLose; 	
 	}
