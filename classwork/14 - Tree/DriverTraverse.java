dec22ef674e5e13df712e44c9e2ec7ea6cddb226
RosaneWeiss <67014774+RosaneWeiss@users.noreply.github.com>
2020-07-20 18:21:17 -0400

    traverse completed

--- classwork/14 - Tree/DriverTraverse.java
+++ classwork/14 - Tree/DriverTraverse.java
@@ -7,19 +7,21 @@ public class DriverTraverse {
 	BSTree t = new BSTree();
 	
 	t.insert(10);
-	t.traverse();
+	
 	t.insert(20);
-	t.traverse();
+	
 	t.insert(5);
-	t.traverse();
+	
 	t.insert(7);
-	t.traverse();
+	
 	t.insert(8);
-	t.traverse();
+	
 	t.insert(3);
-	t.traverse();
+	
 	t.insert(25);
 	
-	t.traverse();
+	t.preorderTraverse();
+	t.postorderTraverse();
+	t.inorderTraverse();
 	}
 }
